a745b09e5946e552c86eb3ae61d364e20c0c01dc
Tobias <t-render-man@sc.rr.com>
2019-05-13 20:27:11 -0400

    update preferences

--- js/pref.js
+++ js/pref.js
@@ -11,4 +11,49 @@ var prefInit = function () {
     $("#username")[0].value = user.name;
     M.updateTextFields();
 
+    $("#themeToggle")[0].checked = user.pref.theme;
+
+
+    $("#prefSave").on("click", updatePref);
+
+
+
+    applyPref();
+};
+
+var applyPref = function () {
+    setUserMenu();
+
+    var userId = auth.currentUser.uid;
+    var user = currentUserInfo[userId];
+
+    if (user.pref.theme) {
+        $("body")[0].classList.remove("lighten");
+        $("body")[0].classList.add("dark");
+    } else {
+        $("body")[0].classList.remove("dark");
+        $("body")[0].classList.add("lighten");
+    }
+
+};
+
+var updatePref = function () {
+    var userId = auth.currentUser.uid;
+    var user = currentUserInfo[userId];
+
+    //get info
+    var newName = $("#username")[0].value;
+    var newTheme = $("#themeToggle")[0].checked;
+
+    user.name = newName;
+    user.pref.theme = newTheme;
+
+    db.collection("users").doc("users").update({
+            [userId]: user
+        })
+        .then(function () {
+            console.log("Document successfully updated!");
+            applyPref();
+        });
+
 };
