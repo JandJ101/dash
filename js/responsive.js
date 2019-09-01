ab815df0b500db8d64f568ac59a7d05997f85ba5
Tobias <t-render-man@sc.rr.com>
2019-09-01 16:59:59 -0400

    responsize design

---
+++ js/responsive.js
@@ -0,0 +1,43 @@
+var handleViewerResize = function() {
+  var width = window.innerWidth;
+
+  var isSmall = false;
+
+  if (width < 800) {
+    isSmall = true;
+    console.log("width");
+  }
+
+  if (isSmall) {
+    $("#viewer")[0].classList.remove("bigViewer");
+    $("#viewer")[0].classList.add("smallViewer");
+
+    $("#viewer")[0].classList.add("scroller");
+    $("#viewer")[0].classList.add("scrollbar");
+
+    $("#comments")[0].classList.remove("scroller");
+    $("#comments")[0].classList.remove("scrollbar");
+    $("#comments")[0].classList.remove("h100");
+
+    $("#infoContainer")[0].classList.remove("scroller");
+    $("#infoContainer")[0].classList.remove("scrollbar");
+    $("#infoContainer")[0].classList.remove("h100");
+  } else {
+    $("#viewer")[0].classList.remove("smallViewer");
+    $("#viewer")[0].classList.add("bigViewer");
+
+    $("#viewer")[0].classList.remove("scroller");
+    $("#viewer")[0].classList.remove("scrollbar");
+
+    $("#comments")[0].classList.add("scroller");
+    $("#comments")[0].classList.add("scrollbar");
+    $("#comments")[0].classList.add("h100");
+
+    $("#infoContainer")[0].classList.add("scroller");
+    $("#infoContainer")[0].classList.add("scrollbar");
+    $("#infoContainer")[0].classList.add("h100");
+  }
+};
+
+$(window).resize(handleViewerResize);
+$(document).ready(handleViewerResize);
--- js/sizeViewer.js
+++ js/sizeViewer.js
@@ -1,59 +1,52 @@
-var getRatio = function (w, h) {
-    var r = 16 / 9;
-    var wid, hi;
+var getRatio = function(w, h) {
+  var r = 16 / 9;
+  var wid, hi;
 
-    if (w) {
-        wid = w;
-        hi = w / r;
-    } else {
-        hi = h;
-        wid = h * r;
-    }
+  if (w) {
+    wid = w;
+    hi = w / r;
+  } else {
+    hi = h;
+    wid = h * r;
+  }
 
-    var val = {};
-    val.h = hi;
-    val.w = wid;
+  var val = {};
+  val.h = hi;
+  val.w = wid;
 
-    return (val);
-
+  return val;
 };
 
 var cont, hc, wc, vid, hvideo, wvideo;
 
-var resizeView = function () {
-    if (currentType == "video") {
-        cont = $("#videoContainer")[0];
-        hc = cont.offsetHeight;
-        wc = cont.offsetWidth;
+var resizeView = function() {
+  if (currentType == "video") {
+    cont = $("#videoContainer")[0];
+    hc = cont.offsetHeight;
+    wc = cont.offsetWidth;
 
-        vid = $("#mainVideo")[0];
-        hvideo = vid.offsetHeight;
-        wvideo = vid.offsetWidth;
+    vid = $("#mainVideo")[0];
+    hvideo = vid.offsetHeight;
+    wvideo = vid.offsetWidth;
 
-        var wh;
-        var cwh = getRatio(wc);
+    var wh;
+    var cwh = getRatio(wc);
 
+    //cwh.h > hc
+    if (false) {
+      wh = getRatio(null, hc);
 
-        if (cwh.h > hc) {
-            wh = getRatio(null, hc);
+      vid.style.height = wh.h;
+      vid.style.width = wh.w;
+    } else {
+      wh = getRatio(wc, null);
 
-            vid.style.height = wh.h;
-            vid.style.width = wh.w;
-
-        } else {
-            wh = getRatio(wc, null);
-
-            vid.style.height = wh.h;
-            vid.style.width = wh.w;
-
-
-        }
-
+      vid.style.height = wh.h;
+      vid.style.width = wh.w;
     }
-
+  }
 };
 
-$(document).ready(function () {
-    $(window).resize(resizeView);
-
-})
+$(document).ready(function() {
+  $(window).resize(resizeView);
+});
