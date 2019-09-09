502366219f77ffeb832a5a90c6243272f4cad850
jandjtv <jandjtv@outlook.com>
2019-09-09 16:26:11 -0400

    video thumbnails

--- js/thumb.js
+++ js/thumb.js
@@ -18,93 +18,70 @@ function dataURItoBlob(dataURI) {
     return new Blob([ia], {
         type: mimeString
     });
-}
-
-//var makeThumbanil = function (file) {
-//
-//    var _CANVAS = document.querySelector("#video-canvas"),
-//        _CTX = _CANVAS.getContext("2d"),
-//        _VIDEO = document.querySelector("#main-video");
-//
-//
-//    // Object Url as the video source
-//    document.querySelector("#main-video source").setAttribute('src', URL.createObjectURL(file));
-//
-//    // Load the video and show it
-//    _VIDEO.load();
-//    _VIDEO.style.display = 'inline';
-//
-//    // Load metadata of the video to get video duration and dimensions
-//    _VIDEO.addEventListener('loadedmetadata', function () {
-//        console.log(_VIDEO.duration);
-//        var video_duration = _VIDEO.duration,
-//            duration_options_html = '';
-//
-//        _VIDEO.currentTime = Math.floor(Math.floor(_VIDEO.duration) / 2);
-//        _CANVAS.width = _VIDEO.videoWidth;
-//        _CANVAS.height = _VIDEO.videoHeight;
-//
-//        _CTX.drawImage(_VIDEO, 0, 0, _VIDEO.videoWidth, _VIDEO.videoHeight);
-//
-//    });
-//
-//    setTimeout(function () {
-//        _CTX.drawImage(_VIDEO, 0, 0, _VIDEO.videoWidth, _VIDEO.videoHeight);
-//        console.log(_CANVAS.toDataURL());
-//
-//
-//    }, 500);
-//
-//};
-
-var theFile;
-
-var video, canvas, img;
-
-var makeThumb = function (vid) {
-
-
-
-    $("#videoId")[0].addEventListener('loadedmetadata', function () {
-
-        var video = document.getElementById('videoId');
-        var canvas = document.getElementById('canvasId');
-        var img = document.getElementById('imgId');
-        draw(video, canvas, img);
-
-        video.pause();
-
-
-
-
-        function draw(video, canvas, img) {
-            var context = canvas.getContext('2d');
-            context.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-
-            setTimeout(function () {
-                var dataURL = canvas.toDataURL("image/jpg");
-                img.setAttribute('src', dataURL);
-                return (dataURL);
-            }, 500);
-
-        };
-    });
-
-
-
-    $("#videoId source")[0].src = URL.createObjectURL(vid);
-    $("#videoId")[0].load();
 };
 
-$(document).ready(function () {
+// var theFile;
 
-    $("#file-input")[0].addEventListener("change", function () {
-        var theFile = $("#file-input")[0].files[0];
-        console.log(makeThumb(theFile));
-    });
-
-});
+// var video, canvas, img;
 
 
-//ready(makeThumb);
+// var dataThing;
+
+// function capture() {
+//     var canvas1 = document.getElementById('canvasId');
+//     var video1 = document.getElementById('videoId');
+//     canvas1.getContext('2d').drawImage(video1, 0, 0, canvas1.width, canvas1.height);
+//     dataThing = canvas1.toDataURL("image/jpg");
+//     return (canvas1.toDataURL("image/jpg"));
+// }
+
+
+// var makeThumb = function(vid) {
+
+
+
+//     $("#videoId")[0].addEventListener('loadedmetadata', function() {
+
+//         console.log("load");
+
+//         var video = document.getElementById('videoId');
+//         var canvas = document.getElementById('canvasId');
+//         var img = document.getElementById('imgId');
+//         draw(video, canvas, img);
+
+//         video.pause();
+
+
+
+
+//         function draw(video, canvas, img) {
+//             var context = canvas.getContext('2d');
+//             context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+
+//             setTimeout(function() {
+//                 var dataURL = canvas.toDataURL("image/jpg");
+//                 img.setAttribute('src', dataURL);
+//                 return (dataURL);
+//             }, 500);
+
+//         };
+//     });
+
+
+
+//     $("#videoId source")[0].src = URL.createObjectURL(vid);
+//     $("#videoId")[0].load();
+// };
+
+// $(document).ready(function() {
+
+//     $("#file-input")[0].addEventListener("change", function() {
+//         var theFile = $("#file-input")[0].files[0];
+//         console.log(makeThumb(theFile));
+//     });
+
+// });
+
+
+// //ready(makeThumb);
